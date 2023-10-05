783e0527403e0a50223a2a84b045973915b73ec5
Waeltalbii <waeltlb14@gmail.com>
2023-10-05 07:45:05 +0100

    DokerFile

---
+++ src/main/java/testdevops/com/testdevops/TestdevopsApplication.java
@@ -0,0 +1,13 @@
+package testdevops.com.testdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/testdevops/com/testdevops/TestdevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package testdevops.com.testdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
